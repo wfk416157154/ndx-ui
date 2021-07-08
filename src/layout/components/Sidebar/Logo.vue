16b2c6a00c0cd11ef0ca1224228466589bedfa64
yuanbing <602341429@qq.com>
2021-07-08 17:33:44 +0800

    logo替换

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -14,7 +14,7 @@
 </template>
 
 <script>
-  import logoImg from '@/assets/logo/logo.png'
+  import logoImg from '@/assets/logo/logo2.jpg'
   import variables from '@/assets/styles/variables.scss'
 
   export default {
