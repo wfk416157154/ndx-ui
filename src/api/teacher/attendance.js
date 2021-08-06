2f04703a540081736aa9af98065f3b750f4b7adf
wjt <1510499112@qq.com>
2021-08-06 13:57:17 +0800

    请求路径修改

--- src/api/teacher/attendance.js
+++ src/api/teacher/attendance.js
@@ -3,7 +3,7 @@ import request from '@/utils/request'
 // 查询老师考勤列表
 export function listAttendance(query) {
   return request({
-    url: '/teacher/attendance/list',
+    url: '/teacher/teacherAttendance/list',
     method: 'get',
     params: query
   })
@@ -12,7 +12,7 @@ export function listAttendance(query) {
 // 查询老师考勤详细
 export function getAttendance(id) {
   return request({
-    url: '/teacher/attendance/' + id,
+    url: '/teacher/teacherAttendance/' + id,
     method: 'get'
   })
 }
@@ -20,7 +20,7 @@ export function getAttendance(id) {
 // 新增老师考勤
 export function addAttendance(data) {
   return request({
-    url: '/teacher/attendance',
+    url: '/teacher/teacherAttendance',
     method: 'post',
     data: data
   })
@@ -29,7 +29,7 @@ export function addAttendance(data) {
 // 修改老师考勤
 export function updateAttendance(data) {
   return request({
-    url: '/teacher/attendance',
+    url: '/teacher/teacherAttendance',
     method: 'put',
     data: data
   })
@@ -38,7 +38,7 @@ export function updateAttendance(data) {
 // 删除老师考勤
 export function delAttendance(id) {
   return request({
-    url: '/teacher/attendance/' + id,
+    url: '/teacher/teacherAttendance/' + id,
     method: 'delete'
   })
 }
