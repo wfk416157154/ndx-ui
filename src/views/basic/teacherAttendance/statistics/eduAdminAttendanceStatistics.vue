4d49a36f04d7efe91a9b405c374f1350ba2d53a8
wjt <1510499112@qq.com>
2021-08-19 09:35:08 +0800

    代码还原到提交状态时

--- src/views/basic/teacherAttendance/statistics/eduAdminAttendanceStatistics.vue
+++ src/views/basic/teacherAttendance/statistics/eduAdminAttendanceStatistics.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="eduAdminAttendanceStatistics">
-    <el-form
-      ref="form"
-      :model="StatisticsForm"
-      :rules="rulesForm"
-      :inline="true"
-      label-width="100px"
-    >
+    <el-form ref="form" :model="StatisticsForm" :rules="rulesForm" :inline="true" label-width="100px">
       <el-form-item label="开始日期" prop="startDate">
         <el-date-picker
           v-model="StatisticsForm.startDate"
@@ -24,13 +18,13 @@
         ></el-date-picker>
       </el-form-item>
       <el-form-item label="校区" prop="xqid">
-        <el-select
-          v-model="StatisticsForm.xqid"
-          filterable
-          placeholder="请选择校区名称"
-          @change="xqmcOnChange"
-        >
-          <el-option v-for="item in schoolList" :key="item.id" :label="item.xxmc" :value="item.id"></el-option>
+        <el-select v-model="StatisticsForm.xqid" filterable placeholder="请选择校区名称" @change="xqmcOnChange" >
+          <el-option
+            v-for="item in schoolList"
+            :key="item.id"
+            :label="item.xxmc"
+            :value="item.id"
+          ></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="日语班级" prop="bjid">
@@ -44,8 +38,8 @@
         </el-select>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" icon="el-icon-search" @click="queryResult">查询</el-button>
-        <el-button icon="el-icon-refresh" @click="resetQuery">重置</el-button>
+        <el-button type="primary" icon="el-icon-search"  @click="queryResult">查询</el-button>
+        <el-button icon="el-icon-refresh"  @click="resetQuery">重置</el-button>
       </el-form-item>
     </el-form>
 
@@ -59,12 +53,7 @@
         ></el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
-            <el-button
-              size="mini"
-              type="primary"
-              icon="el-icon-search"
-              @click="pageTurnTeacherAttendanceDetail(scope.$index, scope.row)"
-            >查看</el-button>
+            <el-button size="mini" type="primary" icon="el-icon-search" @click="pageTurnTeacherAttendanceDetail(scope.$index, scope.row)">查看</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -81,142 +70,134 @@
 </template>
 
 <script>
-import {
-  jwColumnNamelist,
-  jwTeaAttenDatalist
-} from "@/api/basic/teacherAttendance";
-import { listBjclass } from "@/api/basic/bjclass";
-import { listSchool } from "@/api/basic/school";
-export default {
-  data() {
-    return {
-      StatisticsForm: {
-        startDate: null,
-        endDate: null,
-        teacherId: null,
-        xqid: null,
-        bjid: null,
-        pageNum: 1,
-        pageSize: 10
-      },
-      rulesForm: {
-        startDate: [
-          { required: true, message: "请选择开始日期", trigger: "blur" }
-        ],
-        endDate: [
-          { required: true, message: "请选择结束日期", trigger: "blur" }
-        ]
-      },
-      total: 0,
-      jwColumnNameItem: [],
-      jwTeaAttenDataItem: [],
-      //校区列表
-      schoolList: [],
-      // 班级信息
-      queryBjclassList: [],
-      limitDays: 31 // 查询范围最多可允许的天数
-    };
-  },
-  created() {
-    this.queryJwTeacherAttendanceColumnNameList();
-    this.getLimitDays();
-    // 获取校区
-    listSchool().then(response => {
-      this.schoolList = response.rows;
-      if (response.rows.length == 1) {
-        // 当只有一个校区则自动赋值
-        this.StatisticsForm.xqid = response.rows[0].id;
-      }
-    });
-  },
-  methods: {
-    /** 重置按钮操作 */
-    resetQuery() {
-      this.resetForm("form");
+  import {
+    jwColumnNamelist,
+    jwTeaAttenDatalist
+  } from "@/api/basic/teacherAttendance";
+  import {
+    listBjclass,
+  } from "@/api/basic/bjclass";
+  import { listSchool } from "@/api/basic/school";
+  export default {
+    data() {
+      return {
+        StatisticsForm: {
+          startDate: null,
+          endDate: null,
+          teacherId: null,
+          xqid: null,
+          bjid: null,
+          pageNum: 1,
+          pageSize: 10
+        },
+        rulesForm:{
+          startDate: [
+            { required: true, message: "请选择开始日期", trigger: "blur" }
+          ],
+          endDate: [
+            { required: true, message: "请选择结束日期", trigger: "blur" }
+          ],
+        },
+        total: 0,
+        jwColumnNameItem: [],
+        jwTeaAttenDataItem: [],
+        //校区列表
+        schoolList: [],
+        // 班级信息
+        queryBjclassList:[],
+        limitDays:31 // 查询范围最多可允许的天数
+      };
     },
-    /* 选择校区触发 */
-    xqmcOnChange(id) {
-      listBjclass({ kzzd1: id }).then(response => {
-        this.queryBjclassList = response.rows;
-      });
-    },
-    /**
-     * 从参数配置中获取限制的最大日期范围天数
-     */
-    getLimitDays() {
-      this.getConfigKey("Limit_days").then(res => {
-        this.limitDays = res.msg;
-      });
-    },
-    /* 查询教务主页面的动态列名 */
-    queryJwTeacherAttendanceColumnNameList() {
-      jwColumnNamelist().then(res => {
-        if (res.code == 200) {
-          this.jwColumnNameItem = res.data;
+    created() {
+      this.queryJwTeacherAttendanceColumnNameList();
+      this.getLimitDays();
+      // 获取校区
+      listSchool().then(response => {
+        this.schoolList = response.rows;
+        if (response.rows.length==1){// 当只有一个校区则自动赋值
+          this.StatisticsForm.xqid=response.rows[0].id
         }
       });
     },
-    /* 点击查询按钮 */
-    queryResult() {
-      this.$refs.form.validate(valid => {
-        if (valid) {
-          let startDate = this.StatisticsForm.startDate;
-          let endDate = this.StatisticsForm.endDate;
-          let date1 = new Date(startDate);
-          let date2 = new Date(endDate);
-          let date3 =
-            (date2.getTime() / 1000 - date1.getTime() / 1000) / 60 / 60 / 24;
-          if (date3 > this.limitDays) {
-            this.msgError("查询考勤最大天数不得超过" + this.limitDays + "天");
-            return;
+    methods: {
+      /** 重置按钮操作 */
+      resetQuery() {
+        this.resetForm("form");
+      },
+      /* 选择校区触发 */
+      xqmcOnChange(id){
+        listBjclass({kzzd1:id}).then(response => {
+          this.queryBjclassList = response.rows
+        });
+      },
+      /**
+       * 从参数配置中获取限制的最大日期范围天数
+       */
+      getLimitDays(){
+        this.getConfigKey("Limit_days").then(res => {
+          this.limitDays=res.msg;
+        })
+      },
+      /* 查询教务主页面的动态列名 */
+      queryJwTeacherAttendanceColumnNameList() {
+        jwColumnNamelist().then(res => {
+          if (res.code == 200) {
+            this.jwColumnNameItem = res.data;
           }
-          this.StatisticsForm.startDate = this.formatDate(date1);
-          this.StatisticsForm.endDate = this.formatDate(date2);
-          jwTeaAttenDatalist(this.StatisticsForm).then(res => {
-            this.jwTeaAttenDataItem = res.rows;
-            this.total = res.total;
-          });
+        });
+      },
+      /* 点击查询按钮 */
+      queryResult() {
+        this.$refs.form.validate(valid => {
+          if (valid) {
+            let startDate = this.StatisticsForm.startDate;
+            let endDate = this.StatisticsForm.endDate;
+            let date1 = new Date(startDate);
+            let date2 = new Date(endDate);
+            let date3 =(date2.getTime() / 1000 - date1.getTime() / 1000)/ 60 / 60 / 24;
+            if (date3 > this.limitDays) {
+              this.msgError("查询考勤最大天数不得超过"+this.limitDays+"天");
+              return;
+            }
+            this.StatisticsForm.startDate =this.formatDate(date1);;
+            this.StatisticsForm.endDate =this.formatDate(date2);
+            jwTeaAttenDatalist(this.StatisticsForm).then(res => {
+              this.jwTeaAttenDataItem = res.rows;
+              this.total = res.total;
+            });
+          }
+        });
+      },
+      /* 日期格式化 */
+      formatDate(date){
+        if(null!=date){
+          return date.getFullYear() +"-" +(date.getMonth()+1) + "-" + date.getDate();
         }
-      });
-    },
-    /* 日期格式化 */
-    formatDate(date) {
-      if (null != date) {
-        return (
-          date.getFullYear() +
-          "-" +
-          (date.getMonth() + 1) +
-          "-" +
-          date.getDate()
-        );
+        return null;
+      },
+      /**
+       * 跳转到老师考勤统计详情页
+       * @param index
+       * @param row
+       */
+      pageTurnTeacherAttendanceDetail(index, row) {
+        this.getConfigKey("teacherAttendanceStatistics").then(res => {
+          if (res.code == 200) {
+            this.$router.push({
+              path:res.msg +JSON.stringify({ lsid: row.lsid, params: this.StatisticsForm })
+            });
+          }
+        });
       }
-      return null;
-    },
-    /**
-     * 跳转到老师考勤统计详情页
-     * @param index
-     * @param row
-     */
-    pageTurnTeacherAttendanceDetail(index, row) {
-      this.getConfigKey("teacherAttendanceStatistics").then(res => {
-        if (res.code == 200) {
-          this.$router.push({
-            path:
-              res.msg +
-              JSON.stringify({ lsid: row.lsid, params: this.StatisticsForm })
-          });
-        }
-      });
     }
-  }
-};
+  };
 </script>
 
 <style lang="scss" scoped>
-.eduAdminAttendanceStatistics {
-  width: 100%;
-  height: 100%;
-  padding: 40px;
-  box-sizing: border-box;
-}
+  .eduAdminAttendanceStatistics {
+    width: 100%;
+    height: 100%;
+    padding: 40px;
+    box-sizing: border-box;
+  }
 </style>
--- src/views/basic/teacherAttendance/statistics/teacherAttendanceStatistics.vue
+++ src/views/basic/teacherAttendance/statistics/teacherAttendanceStatistics.vue
@@ -5,7 +5,7 @@
         <el-date-picker
           v-model="StatisticsForm.startDate"
           type="date"
-          format="yyyy-MM-dd"
+          value-format="yyyy-MM-dd"
           placeholder="开始日期"
         ></el-date-picker>
       </el-form-item>
@@ -13,7 +13,7 @@
         <el-date-picker
           v-model="StatisticsForm.endDate"
           type="date"
-          format="yyyy-MM-dd"
+          value-format="yyyy-MM-dd"
           placeholder="结束日期"
         ></el-date-picker>
       </el-form-item>
@@ -75,155 +75,155 @@
 </template>
 
 <script>
-import {
-  teacherAttendanceStatistics,
-  teacherAttendanceStatisticsList
-} from "@/api/basic/teacherAttendance";
-import {
-  listBjclass,
-} from "@/api/basic/bjclass";
-export default {
-  data() {
-    return {
-      /* 老师姓名和班级名称+日期范围 */
-      teacherAndBjmc:"",
-      StatisticsForm: {
-        startDate: "",
-        endDate: "",
-        teacherId: this.$store.state.user.glrid,
-        xqid: "",
-        bjid: ""
-      },
-      rulesForm:{
-        startDate: [
-          { required: true, message: "请选择开始日期", trigger: "blur" }
-        ],
-        endDate: [
-          { required: true, message: "请选择结束日期", trigger: "blur" }
-        ],
-      },
-      detailList: [],
-      resultList: [],
-      detailItem: [],
-      resultItem: [],
-      statisticsProject: [],
-      statisticsResultType: [],
-      queryBjclassList:[],// 班级列表
-      limitDays:31 // 查询范围最多可允许的天数
-    };
-  },
-  created() {
-    this.getLimitDays();//从参数配置中获取限制的最大日期范围天数
-    this.getColumnNameList();// 获取动态列名
-    this.pageTurnInvokeMethod();// 从列表页面点击【查看】按钮跳转过来时执行的方法
-    this.getDicts("statistics_project").then(res => {
-      this.statisticsProject = res.data;
-    });
-    this.getDicts("statistics_result_type").then(res => {
-      this.statisticsResultType = res.data;
-    });
-  },
-  methods: {
-    /** 重置按钮操作 */
-    resetQuery() {
-      this.resetForm("StatisticsForm");
+  import {
+    teacherAttendanceStatistics,
+    teacherAttendanceStatisticsList
+  } from "@/api/basic/teacherAttendance";
+  import {
+    listBjclass,
+  } from "@/api/basic/bjclass";
+  export default {
+    data() {
+      return {
+        /* 老师姓名和班级名称+日期范围 */
+        teacherAndBjmc:"",
+        StatisticsForm: {
+          startDate: "",
+          endDate: "",
+          teacherId: this.$store.state.user.glrid,
+          xqid: "",
+          bjid: ""
+        },
+        rulesForm:{
+          startDate: [
+            { required: true, message: "请选择开始日期", trigger: "blur" }
+          ],
+          endDate: [
+            { required: true, message: "请选择结束日期", trigger: "blur" }
+          ],
+        },
+        detailList: [],
+        resultList: [],
+        detailItem: [],
+        resultItem: [],
+        statisticsProject: [],
+        statisticsResultType: [],
+        queryBjclassList:[],// 班级列表
+        limitDays:31 // 查询范围最多可允许的天数
+      };
     },
-    /* 获取动态列名 */
-    getColumnNameList() {
-      teacherAttendanceStatistics().then(res => {
-        if (res.code == 200) {
-          this.detailList = res.data.detailList;
-          this.resultList = res.data.resultList;
-        }
+    created() {
+      this.getLimitDays();//从参数配置中获取限制的最大日期范围天数
+      this.getColumnNameList();// 获取动态列名
+      this.pageTurnInvokeMethod();// 从列表页面点击【查看】按钮跳转过来时执行的方法
+      this.getDicts("statistics_project").then(res => {
+        this.statisticsProject = res.data;
+      });
+      this.getDicts("statistics_result_type").then(res => {
+        this.statisticsResultType = res.data;
       });
     },
-    /* 点击查询按钮触发 */
-    queryData() {
-      this.$refs.StatisticsForm.validate(valid => {
-        if (valid) {
+    methods: {
+      /** 重置按钮操作 */
+      resetQuery() {
+        this.resetForm("StatisticsForm");
+      },
+      /* 获取动态列名 */
+      getColumnNameList() {
+        teacherAttendanceStatistics().then(res => {
+          if (res.code == 200) {
+            this.detailList = res.data.detailList;
+            this.resultList = res.data.resultList;
+          }
+        });
+      },
+      /* 点击查询按钮触发 */
+      queryData() {
+        this.$refs.StatisticsForm.validate(valid => {
+          if (valid) {
+            let no = this.getDate();
+            if (!no) {
+              this.getStatisticsList();
+            }
+          }
+        })
+      },
+      /* 从列表页面点击【查看】按钮跳转过来时执行的方法 */
+      pageTurnInvokeMethod(){
+        if (this.$route.params.list != ":list") {// 非老师角色查看
+          let result = JSON.parse(this.$route.params.list);// 转成json的格式进行页面传参
+          this.StatisticsForm.xqid=result.params.xqid;// 父页面传过来的校区id
+          this.StatisticsForm.bjid=result.params.bjid;// 父页面传过来的班级id
+          this.StatisticsForm.teacherId = result.lsid;// 父页面传过来的老师id
+          this.StatisticsForm.startDate = result.params.startDate;// 父页面传过来的开始日期
+          this.StatisticsForm.endDate = result.params.endDate;// 父页面传过来的结束日期
+          let bjParams={
+            lsid:this.StatisticsForm.teacherId,// 老师id
+            id:this.StatisticsForm.bjid // 日语班级id
+          }
+          // 查询班级列表
+          listBjclass(bjParams).then(response => {// 根据父页面传过来的老师id和班级id进行查询
+            this.queryBjclassList = response.rows
+          });
+          // 校验选择的日期范围
           let no = this.getDate();
           if (!no) {
             this.getStatisticsList();
           }
+        }else{// 老师选择班级时
+          listBjclass().then(response => {
+            this.queryBjclassList = response.rows
+          });
         }
-      })
-    },
-    /* 从列表页面点击【查看】按钮跳转过来时执行的方法 */
-    pageTurnInvokeMethod(){
-      if (this.$route.params.list != ":list") {// 非老师角色查看
-        let result = JSON.parse(this.$route.params.list);// 转成json的格式进行页面传参
-        this.StatisticsForm.xqid=result.params.xqid;// 父页面传过来的校区id
-        this.StatisticsForm.bjid=result.params.bjid;// 父页面传过来的班级id
-        this.StatisticsForm.teacherId = result.lsid;// 父页面传过来的老师id
-        this.StatisticsForm.startDate = result.params.startDate;// 父页面传过来的开始日期
-        this.StatisticsForm.endDate = result.params.endDate;// 父页面传过来的结束日期
-        let bjParams={
-          lsid:this.StatisticsForm.teacherId,// 老师id
-          id:this.StatisticsForm.bjid // 日语班级id
-        }
-        // 查询班级列表
-        listBjclass(bjParams).then(response => {// 根据父页面传过来的老师id和班级id进行查询
-          this.queryBjclassList = response.rows
-        });
-        // 校验选择的日期范围
-        let no = this.getDate();
-        if (!no) {
-          this.getStatisticsList();
-        }
-      }else{// 老师选择班级时
-        listBjclass().then(response => {
-          this.queryBjclassList = response.rows
-        });
-      }
-    },
-    /* 获取统计的数据 */
-    getStatisticsList() {
-      teacherAttendanceStatisticsList(this.StatisticsForm).then(res => {
-        if (res.code == 200) {
-          this.teacherAndBjmc=res.data.title;
-          this.resultItem = res.data.resultList;
-          this.detailItem = res.data.detailList;
-          for (let j = 0; j < this.resultItem.length; j++) {
-            for (let i = 0; i < this.statisticsProject.length; i++) {
-              if (this.resultItem[j].project ==this.statisticsProject[i].dictValue) {
-                this.resultItem[j].project = this.statisticsProject[i].dictLabel;
+      },
+      /* 获取统计的数据 */
+      getStatisticsList() {
+        teacherAttendanceStatisticsList(this.StatisticsForm).then(res => {
+          if (res.code == 200) {
+            this.teacherAndBjmc=res.data.title;
+            this.resultItem = res.data.resultList;
+            this.detailItem = res.data.detailList;
+            for (let j = 0; j < this.resultItem.length; j++) {
+              for (let i = 0; i < this.statisticsProject.length; i++) {
+                if (this.resultItem[j].project ==this.statisticsProject[i].dictValue) {
+                  this.resultItem[j].project = this.statisticsProject[i].dictLabel;
+                }
               }
             }
           }
+        });
+      },
+      /**
+       * 判断选择的日期范围是否是限制的范围内
+       * @returns {boolean}
+       */
+      getDate() {
+        let date1 = new Date(this.StatisticsForm.startDate);
+        let date2 = new Date(this.StatisticsForm.endDate);
+        let date3 =(date2.getTime() / 1000 - date1.getTime() / 1000)/ 60 / 60 / 24;
+        if (date3 > this.limitDays) {
+          this.msgError("查询考勤最大天数不得超过"+this.limitDays+"天");
+          return true;
         }
-      });
-    },
-    /**
-     * 判断选择的日期范围是否是限制的范围内
-     * @returns {boolean}
-     */
-    getDate() {
-      let date1 = new Date(this.StatisticsForm.startDate);
-      let date2 = new Date(this.StatisticsForm.endDate);
-      let date3 =(date2.getTime() / 1000 - date1.getTime() / 1000)/ 60 / 60 / 24;
-      if (date3 > this.limitDays) {
-        this.msgError("查询考勤最大天数不得超过"+this.limitDays+"天");
-        return true;
-      }
-      return false;
-    },
-    /**
-     * 从参数配置中获取限制的最大日期范围天数
-     */
-    getLimitDays(){
-      this.getConfigKey("Limit_days").then(res => {
-        this.limitDays=res.msg;
-      })
-    },
-  }
-};
+        return false;
+      },
+      /**
+       * 从参数配置中获取限制的最大日期范围天数
+       */
+      getLimitDays(){
+        this.getConfigKey("Limit_days").then(res => {
+          this.limitDays=res.msg;
+        })
+      },
+    }
+  };
 </script>
 
 <style lang="scss" scoped>
-.teacherAttendanceStatistics {
-  width: 100%;
-  height: 100%;
-  padding: 40px;
-  box-sizing: border-box;
-}
+  .teacherAttendanceStatistics {
+    width: 100%;
+    height: 100%;
+    padding: 40px;
+    box-sizing: border-box;
+  }
 </style>
