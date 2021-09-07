0843d21689300d7103c506a80f1b2ed70d6d6a8d
yuanbing <602341429@qq.com>
2021-09-07 17:24:35 +0800

    班级成绩

--- src/views/basic/classPerformance/classPerformanceDetails.vue
+++ src/views/basic/classPerformance/classPerformanceDetails.vue
@@ -90,6 +90,7 @@
                 style="background : red; display : inline-block ; width :100%;color : #fff"
               >{{scope.row[item.prop]}}</span>
               <span v-if="scope.row[item.colour] == 4">{{scope.row[item.prop]}}</span>
+              <span v-if="scope.row[item.colour] == 5">{{scope.row[item.prop]}}</span>
             </div>
           </template>
         </el-table-column>
