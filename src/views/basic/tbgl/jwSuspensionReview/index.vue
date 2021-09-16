bdcbde4d2397b5350b28b7647f0696a38eac1672
yuanbing <602341429@qq.com>
2021-09-16 13:55:36 +0800

    免责,休学增删改查

--- src/views/basic/tbgl/jwSuspensionReview/index.vue
+++ src/views/basic/tbgl/jwSuspensionReview/index.vue
@@ -6,73 +6,235 @@
       :inline="true"
       label-width="80px"
     >
-      <el-form-item label="学校">
-        <el-select v-model="jsSuspensionReviewForm.region" placeholder="请选择负责人">
-          <el-option label="区域一" value="shanghai"></el-option>
+      <el-form-item label="学校" prop="xqid">
+        <el-select
+          v-model="jsSuspensionReviewForm.xqid"
+          filterable
+          placeholder="请选择校区"
+          @change="xqmcOnChange"
+        >
+          <el-option v-for="item in schoolList" :key="item.id" :label="item.xxmc" :value="item.id"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="班级">
-        <el-select v-model="jsSuspensionReviewForm.region" placeholder="请选择负责人">
-          <el-option label="区域一" value="shanghai"></el-option>
+      <el-form-item label="班级" prop="rybjid">
+        <el-select v-model="jsSuspensionReviewForm.rybjid" placeholder="请选择班级">
+          <el-option
+            :label="item.rybjmc"
+            :value="item.id"
+            v-for="(item,index) in classList"
+            :key="index"
+          ></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="学生">
-        <el-select v-model="jsSuspensionReviewForm.region" placeholder="请选择负责人">
-          <el-option label="区域一" value="shanghai"></el-option>
-        </el-select>
+      <el-form-item label="学生" prop="xsxm">
+        <el-input v-model="jsSuspensionReviewForm.xsxm" placeholder="请输入内容"></el-input>
       </el-form-item>
-      <el-form-item label="状态">
-        <el-select v-model="jsSuspensionReviewForm.region" placeholder="请选择负责人">
-          <el-option label="区域一" value="shanghai"></el-option>
+      <el-form-item label="休学状态" prop="shzt">
+        <el-select v-model="jsSuspensionReviewForm.shzt" placeholder="请选择班级状态">
+          <el-option
+            :label="item.dictLabel"
+            :value="item.dictValue"
+            v-for="(item,index) in xxStatusList"
+            :key="index"
+          ></el-option>
         </el-select>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary">查询</el-button>
-        <el-button type="danger">未审批</el-button>
-        <el-button type="success">已审批</el-button>
-        <el-button icon="el-icon-refresh">重置</el-button>
+        <el-button type="primary" @click="getList()">查询</el-button>
+        <el-button type="danger" @click="getList([1])">未审批</el-button>
+        <el-button type="success" @click="getList([2,3])">已审批</el-button>
+        <el-button icon="el-icon-refresh" @click="resetForm('jsSuspensionReviewForm')">重置</el-button>
       </el-form-item>
     </el-form>
 
     <el-table :data="offShiftListList" border style="width: 100%">
-      <el-table-column prop="date" label="学校" width="180"></el-table-column>
-      <el-table-column prop="name" label="班级" width="180"></el-table-column>
-      <el-table-column prop="address" label="老师"></el-table-column>
-      <el-table-column prop="address" label="学生姓名"></el-table-column>
-      <el-table-column prop="address" label="休学时间"></el-table-column>
-      <el-table-column prop="address" label="审核文件"></el-table-column>
+      <el-table-column prop="xxmc" label="学校" width="180"></el-table-column>
+      <el-table-column prop="rybjmc" label="班级" width="180"></el-table-column>
+      <el-table-column prop="lsxm" label="老师"></el-table-column>
+      <el-table-column prop="xsxm" label="学生姓名"></el-table-column>
+      <el-table-column prop="xxyy" label="休学原因" width="300px"></el-table-column>
+      <el-table-column label="休学时间">
+        <template slot-scope="scope">
+          <span>{{scope.row.xskssj}} - {{scope.row.xsjssj}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="shwj" label="审核文件" :width="flexColumnWidth('shwj',offShiftListList)">
+        <template slot-scope="scope">
+          <div class="block" style="display : flex; width : 100% ; height : 100%">
+            <el-image
+              style="width: 60px; height: 60px; margin : 0px 5px"
+              v-for="(item,index) in scope.row.shwj"
+              :key="index"
+              :src="item"
+              :preview-src-list="[item]"
+            >
+              <div
+                slot="error"
+                style="width : 100%; height : 100%; display : flex; align-items : center;background : #eee; font-size : 12px;justify-content:center;color : #c0c4cc"
+                class="image-slot"
+              >
+                <span>加载失败</span>
+              </div>
+            </el-image>
+          </div>
+        </template>
+      </el-table-column>
       <el-table-column label="操作" width="260px">
         <template slot-scope="scope">
-          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">同意</el-button>
-          <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">驳回</el-button>
+          <el-button
+            size="mini"
+            @click="handleDdopt(scope.$index, scope.row)"
+            :disabled="scope.row.shzt != 1"
+          >同意</el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)"
+            :disabled="scope.row.shzt != 1"
+          >驳回</el-button>
         </template>
       </el-table-column>
     </el-table>
 
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="jsSuspensionReviewForm.pageNum"
+      :limit.sync="jsSuspensionReviewForm.pageSize"
+      @pagination="getList"
+    />
+
     <el-dialog title="休学驳回" :visible.sync="dialogVisible" width="30%">
       <div>
-        <el-input type="textarea" :rows="4" placeholder="请输入驳回原因" v-model="textarea"></el-input>
+        <el-input
+          type="textarea"
+          :rows="4"
+          placeholder="请输入驳回原因"
+          v-model="jsSuspensionReviewForm.kzzd1"
+        ></el-input>
       </div>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+        <el-button type="primary" @click="bhSubmit">确 定</el-button>
       </span>
     </el-dialog>
   </div>
 </template>
 
 <script>
+import { listSchool } from "@/api/basic/school";
+import { listBjclass } from "@/api/basic/bjclass";
+import { listXiuxue, updateXiuxue } from "@/api/basic/xiuxue.js";
 export default {
   data() {
     return {
-      jsSuspensionReviewForm: {},
-      offShiftListList: [{}],
-      dialogVisible: false
+      jsSuspensionReviewForm: {
+        pageNum: 1,
+        pageSize: 10,
+        xqid: "",
+        rybjid: "",
+        xsxm: "",
+        shzt: "",
+        shztArr: []
+      },
+      total: 0,
+      offShiftListList: [],
+      dialogVisible: false,
+      classList: [],
+      schoolList: [],
+      xxStatusList: [],
+      __row: {}
     };
   },
+  created() {
+    listSchool().then(response => {
+      this.schoolList = response.rows;
+    });
+    this.getDicts("xiuxue_status").then(res => {
+      this.xxStatusList = res.data;
+    });
+  },
+  mounted() {
+    this.getList();
+  },
   methods: {
-    handleDelete() {
+    // 获取班级
+    xqmcOnChange(xqid) {
+      listBjclass({ kzzd1: xqid }).then(res => {
+        this.classList = res.rows;
+      });
+    },
+    // 数据
+    getList(shztArr = false) {
+      if (shztArr) {
+        this.jsSuspensionReviewForm.shztArr = shztArr;
+      } else {
+        this.jsSuspensionReviewForm.shztArr = [];
+      }
+      listXiuxue(this.jsSuspensionReviewForm).then(res => {
+        this.offShiftListList = res.rows;
+        this.total = res.total;
+      });
+    },
+    // 驳回
+    handleDelete(i, row) {
+      this.__row = row;
       this.dialogVisible = true;
+    },
+    // 重置
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+      this.getList();
+    },
+    // 宽度适配
+    flexColumnWidth(str, tableData) {
+      let arr = [];
+      for (let i = 0; i < tableData.length; i++) {
+        if (tableData[i] && tableData[i][str] && tableData[i][str].length > 0) {
+          tableData.forEach(obj => {
+            if (obj[str] && obj[str].length) arr.push(obj[str].length);
+          });
+        } else {
+          continue;
+        }
+      }
+      return Math.max.call(null, ...arr) * 75;
+    },
+    // 同意
+    handleDdopt(i, row) {
+      this.jsSuspensionReviewForm.id = row.id;
+      this.jsSuspensionReviewForm.shzt = 2;
+      updateXiuxue(this.jsSuspensionReviewForm).then(res => {
+        if (res.code == 200) {
+          this.msgSuccess("成功 : 申请已通过");
+          this.reset();
+          this.getList();
+        }
+      });
+    },
+    //驳回
+    bhSubmit() {
+      this.jsSuspensionReviewForm.id = this.__row.id;
+      this.jsSuspensionReviewForm.shzt = 3;
+      updateXiuxue(this.jsSuspensionReviewForm).then(res => {
+        if (res.code == 200) {
+          this.msgSuccess("成功 : 申请已驳回");
+          this.reset();
+          this.getList();
+        }
+      });
+      this.dialogVisible = false;
+    },
+    reset() {
+      this.jsSuspensionReviewForm = {
+        pageNum: 1,
+        pageSize: 10,
+        xqid: "",
+        rybjid: "",
+        xsxm: "",
+        shzt: "",
+        shztArr: []
+      };
     }
   }
 };
--- src/views/basic/tbgl/lsExemptionApplication/index.vue
+++ src/views/basic/tbgl/lsExemptionApplication/index.vue
@@ -3,44 +3,46 @@
     style="width : 100%;height : 100%;padding : 40px; box-sizing : border-box;text-align : center"
   >
     <h3>免责申请</h3>
-    <div>
+    <div v-if="ifELE">
       <table style="width : 100%;" border="1" cellspacing="0">
         <thead>
           <tr>
             <th>班级</th>
-            <th>汉川二中20级22届</th>
+            <th>{{getInfo.rybjmc}}</th>
           </tr>
         </thead>
         <tbody>
           <tr>
+            <td>学生</td>
+            <td>{{getInfo.xsxm}}</td>
+          </tr>
+          <tr>
             <td>老师</td>
-            <td>张三</td>
+            <td>{{lsName}}</td>
           </tr>
           <tr>
             <td>入班时间</td>
-            <td>2021-1-18</td>
+            <td>{{getInfo.rbsj}}</td>
           </tr>
           <tr>
-            <td>原因</td>
+            <td>免责原因</td>
             <td class="clearfix">
               <div>
-                <el-input type="textarea" :rows="4" placeholder="请输入休学原因" v-model="textarea"></el-input>
+                <el-input type="textarea" :rows="4" placeholder="请输入免责原因" v-model="mzform.mzyy"></el-input>
               </div>
             </td>
           </tr>
           <tr>
             <td>免责资料</td>
             <td>
-              <div style="width : 180px;height : 180px; margin-top : 30px">
+              <div style="margin-top : 30px;float : left">
                 <el-upload
                   :action="upload.imgUrl"
                   :headers="upload.headers"
                   list-type="picture-card"
                   :on-preview="handlePictureCardPreview"
                   :on-remove="handleRemove"
-                  :on-success="jswsSuccess"
-                  :file-list="files1"
-                  :disabled="ifRoleHasPerms"
+                  :on-success="mzSuccess"
                 >
                   <i class="el-icon-plus"></i>
                 </el-upload>
@@ -53,13 +55,78 @@
           <tr>
             <td>备注</td>
             <td>
-              <el-input type="textarea" :rows="4" placeholder="请输入银行卡号" v-model="textarea"></el-input>
+              <el-input type="textarea" :rows="4" placeholder="请输入内容" v-model="mzform.remark"></el-input>
             </td>
           </tr>
           <tr>
             <td>操作</td>
             <td>
-              <el-button type="primary">提交申请</el-button>
+              <el-button type="primary" @click="addExemptionApplication">提交申请</el-button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div v-else>
+      <table style="width : 100%;" border="1" cellspacing="0">
+        <thead>
+          <tr>
+            <th>班级</th>
+            <th>{{getInfo.rybjmc}}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>学生</td>
+            <td>{{getInfo.xsxm}}</td>
+          </tr>
+          <tr>
+            <td>老师</td>
+            <td>{{getInfo.lsxm}}</td>
+          </tr>
+          <tr>
+            <td>入班时间</td>
+            <td>{{getInfo.rbsj}}</td>
+          </tr>
+          <tr>
+            <td>免责原因</td>
+            <td class="clearfix">
+              <div>
+                <el-input type="textarea" :rows="4" placeholder="请输入免责原因" v-model="getInfo.mzyy"></el-input>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>免责资料</td>
+            <td>
+              <div style="margin-top : 30px;float : left">
+                <el-upload
+                  :action="upload.imgUrl"
+                  :headers="upload.headers"
+                  list-type="picture-card"
+                  :on-preview="handlePictureCardPreviewEdit"
+                  :on-remove="handleRemoveEdit"
+                  :on-success="mzSuccessEdit"
+                  :file-list="files1"
+                >
+                  <i class="el-icon-plus"></i>
+                </el-upload>
+                <el-dialog :visible.sync="dialogVisible">
+                  <img width="100%" :src="dialogImageUrl" alt />
+                </el-dialog>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>备注</td>
+            <td>
+              <el-input type="textarea" :rows="4" placeholder="请输入内容" v-model="getInfo.remark"></el-input>
+            </td>
+          </tr>
+          <tr>
+            <td>操作</td>
+            <td>
+              <el-button type="primary" @click="editupdateMianze">提交修改申请</el-button>
             </td>
           </tr>
         </tbody>
@@ -69,13 +136,13 @@
 </template>
     
 <script>
+import { addMianze, updateMianze } from "@/api/basic/mianze";
 import { getToken } from "@/utils/auth";
+import { secretKey } from "@/utils/tools";
+import { addImg, addFile, selectFileList, deleteImg } from "@/api/tool/common";
 export default {
   data() {
     return {
-      radio: "1",
-      textarea: "",
-      value1: "",
       upload: {
         // 是否显示弹出层（用户导入）
         open: false,
@@ -96,17 +163,89 @@ export default {
       },
       dialogVisible: false,
       files1: null,
-      dialogImageUrl: null
+      getInfo: null,
+      dialogImageUrl: null,
+      mzform: {
+        id: ""
+      },
+      ifELE: true
     };
   },
+  created() {
+    if (this.$route.query.query) {
+      this.ifELE = true;
+      this.getInfo = JSON.parse(this.$route.query.query);
+    } else {
+      this.ifELE = false;
+      this.getInfo = JSON.parse(this.$route.query.row);
+      this.getImg();
+    }
+    this.lsid = this.$store.state.user.glrid;
+    this.lsName = this.$store.state.user.nickName;
+  },
   methods: {
-    // 图片预览 大图
+    getImg() {
+      selectFileList({ kzzd1: this.getInfo.id }).then(res => {
+        this["files1"] = res.rows;
+      });
+    },
+    // 休学图片预览
+    handlePictureCardPreviewEdit(file) {
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
+    },
+    //休学图片删除
+    handleRemoveEdit(file) {
+      deleteImg(file.id).then(res => {
+        if (res.code == 200) {
+          this.$message({
+            message: "删除成功",
+            type: "success"
+          });
+          this.getImg();
+        } else {
+          this.$message.error("删除失败");
+        }
+      });
+    },
+    // 休学成功回调
+    mzSuccessEdit(response, file) {
+      if (response.code == 500) {
+        this.msgError("错误 : 上传失败");
+        return;
+      }
+      let data = response.data;
+      data.kzzd1 = this.getInfo.id || secretKey();
+      this.getInfo.id = data.kzzd1;
+      addImg(data).then(res => {
+        file.id = res.data.id;
+      });
+    },
+    editupdateMianze() {
+      this.getImg();
+      if (!this.getInfo.mzyy) {
+        this.msgError("错误 : 请填写免责原因");
+        return;
+      }
+      if (this.files1.length == 0) {
+        this.msgError("错误 : 请上传免责申请资料");
+        return;
+      }
+      this.getInfo.shzt = 1;
+      updateMianze(this.getInfo).then(res => {
+        if (res.code == 200) {
+          this.msgSuccess("成功 : 修改成功");
+          this.$router.go(-1);
+        }
+      });
+    },
+    // 休学图片预览
     handlePictureCardPreview(file) {
       this.dialogImageUrl = file.url;
       this.dialogVisible = true;
     },
-    //图片删除
-    handleRemove(file, fileList) {
+    //休学图片删除
+    handleRemove(file) {
       deleteImg(file.id).then(res => {
         if (res.code == 200) {
           this.$message({
@@ -118,22 +257,41 @@ export default {
         }
       });
     },
-    // 教室卫生图片上传成功回调
-    jswsSuccess(response, file, fileList) {
+    // 休学成功回调
+    mzSuccess(response, file) {
       if (response.code == 500) {
-        this.$notify({
-          message: "上传失败",
-          type: "error"
-        });
+        this.msgError("错误 : 上传失败");
         return;
       }
       let data = response.data;
-      data.kzzd1 = this.ruleForm.jswsFile || secretKey();
-      this.ruleForm.jswsFile = data.kzzd1;
-      this.photoNum1 = fileList.length;
+      data.kzzd1 = this.mzform.id || secretKey();
+      this.mzform.id = data.kzzd1;
       addImg(data).then(res => {
         file.id = res.data.id;
-        this.ifPhotoLimit(this.photoNum1);
+      });
+    },
+    addExemptionApplication() {
+      if (!this.mzform.mzyy) {
+        this.msgError("错误 : 请填写免责原因");
+        return;
+      }
+      if (!this.mzform.id) {
+        this.msgError("错误 : 请上传免责资料");
+        return;
+      }
+      let jsonList = {
+        id: this.mzform.id || secretKey(),
+        lsid: this.lsid,
+        remark: this.mzform.remark,
+        rybjid: this.getInfo.ryb,
+        xsbh: this.getInfo.xsbh,
+        mzyy: this.mzform.mzyy
+      };
+      addMianze(jsonList).then(res => {
+        if (res.code == 200) {
+          this.msgSuccess("成功 : 操作成功");
+          this.$router.go(-1);
+        }
       });
     }
   }
