1fa3c61020e940a93819cdcede91e2b8d80db73d
18772409857 <w1510499112>
2021-12-08 15:24:41 +0800

    新增两个查询接口的方法：查询老师培训过程的笔记图片列表、新增或修改老师培训过程

--- src/api/basic/trainTeacherProcess.js
+++ src/api/basic/trainTeacherProcess.js
@@ -9,6 +9,16 @@ export function listTrainTeacherProcess(query) {
   })
 }
 
+// 查询老师培训过程的笔记图片列表
+export function notesListTrainTeacherProcess(query) {
+  return request({
+    url: '/basic/trainTeacherProcess/notesList',
+    method: 'get',
+    params: query
+  })
+}
+
+
 // 查询老师培训过程详细
 export function getTrainTeacherProcess(id) {
   return request({
@@ -21,6 +31,15 @@ export function getTrainTeacherProcess(id) {
 export function addTrainTeacherProcess(data) {
   return request({
     url: '/basic/trainTeacherProcess',
+    method: 'post',
+    data: data
+  })
+}
+
+// 新增或修改老师培训过程
+export function addOrUpdateTrainTeacherProcess(data) {
+  return request({
+    url: '/basic/trainTeacherProcess/addOrUpdate',
     method: 'post',
     data: data
   })
