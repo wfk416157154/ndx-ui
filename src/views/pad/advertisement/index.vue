0009f1c8972c7772170feb8fd4a009c5495e8632
15327359823 <fei15327359823.163.com>
2022-03-28 08:35:59 +0800

    广告上传图片修改

--- src/views/pad/advertisement/index.vue
+++ src/views/pad/advertisement/index.vue
@@ -194,8 +194,28 @@
         <el-form-item label="广告标题" prop="advertisementTitle">
           <el-input v-model="form.advertisementTitle" placeholder="请输入广告标题" />
         </el-form-item>
-        <el-form-item label="广告内容">
-          <editor v-model="form.advertisementContent" :min-height="220"/>
+        <el-form-item label="文件" prop="id">
+          <el-upload
+            ref="upload"
+            :limit="maxPhotoNum"
+            accept=".*"
+            :headers="upload.headers"
+            :action="upload.url"
+            :disabled="upload.isUploading"
+            :on-remove="handleRemove"
+            :on-progress="handleFileUploadProgress"
+            :on-success="handleFileSuccess"
+            :auto-upload="true"
+            :file-list="wjidFile"
+            drag
+          >
+            <i class="el-icon-upload"></i>
+            <div class="el-upload__text">
+              将文件拖到此处，或
+              <em>点击上传</em>
+            </div>
+            <div class="el-upload__tip" style="color:red" slot="tip">提示：仅允许上传50M以内的文件！</div>
+          </el-upload>
         </el-form-item>
         <el-form-item label="是否发送所有(0:否 1:是)" prop="sffbsy">
           <el-select v-model="form.sffbsy" placeholder="请选择是否发送给所有用户"  >
@@ -277,40 +297,6 @@
       </div>
     </el-dialog>
 
-      <!-- 导入对话框 -->
-      <el-dialog :title="upload.title" :visible.sync="upload.open" width="400px">
-          <el-upload
-                  ref="upload"
-                  :limit="1"
-                  accept=".xlsx, .xls"
-                  :headers="upload.headers"
-                  :action="upload.url + '?updateSupport=' + upload.updateSupport"
-                  :disabled="upload.isUploading"
-                  :on-progress="handleFileUploadProgress"
-                  :on-success="handleFileSuccess"
-                  :auto-upload="false"
-                  drag
-                  v-loading="fullscreenLoading"
-                  element-loading-text="正在进行数据导入·······"
-                  element-loading-spinner="el-icon-loading"
-                  element-loading-background="rgba(0, 0, 0, 0.8)"
-          >
-              <i class="el-icon-upload"></i>
-              <div class="el-upload__text">
-                  将文件拖到此处，或
-                  <em>点击上传</em>
-              </div>
-              <div class="el-upload__tip" slot="tip">
-                  <el-checkbox v-model="upload.updateSupport" />是否更新已经存在的数据
-                  <el-link type="info" style="font-size:12px" @click="importTemplate">下载模板</el-link>
-              </div>
-              <div class="el-upload__tip" style="color:red" slot="tip">提示：仅允许导入“xls”或“xlsx”格式文件！</div>
-          </el-upload>
-          <div slot="footer" class="dialog-footer">
-              <el-button type="primary" v-prevent-re-click :disabled="importBtn" @click="submitFileForm">确 定</el-button>
-              <el-button @click="upload.open = false">取 消</el-button>
-          </div>
-      </el-dialog>
 
   </div>
 
@@ -322,6 +308,8 @@ import { listAdvertisement, getAdvertisement, delAdvertisement, addAdvertisement
 import { getToken } from "@/utils/auth";
 import {listSchool} from "@/api/basic/school";
 import {listTeacher} from "@/api/basic/teacher";
+import {addImg, selectFileList, deleteImg} from "@/api/tool/common";
+import {secretKey} from "@/utils/tools";
 
 export default {
   name: "Advertisement",
@@ -382,21 +370,23 @@ export default {
           { required: true, message: "广告内不能为空", trigger: "blur" }
         ],
       },
-      // 导入参数
+      // 上传参数
       upload: {
-          // 是否显示弹出层
-          open: false,
-          // 弹出层标题
-          title: "导入广告数据",
-          // 是否禁用上传
-          isUploading: false,
-          // 是否更新已经存在的数据
-          updateSupport: 0,
-          // 设置上传的请求头部
-          headers: { Authorization: "Bearer " + getToken() },
-          // 上传的地址
-          url: process.env.VUE_APP_BASE_API + "/pad/advertisement/importData"
+        // 是否显示弹出层
+        open: false,
+        // 弹出层标题
+        title: "上传文件",
+        // 是否禁用上传
+        isUploading: false,
+        // 是否更新已经存在的数据
+        updateSupport: 0,
+        // 设置上传的请求头部
+        headers: {Authorization: "Bearer " + getToken()},
+        // 上传的地址
+        url: process.env.VUE_APP_BASE_API + "/file/upload"
       },
+      // 文件id数组
+      wjidFile: [],
       // 老师列表
       teacherList: [],
       //校区名称
@@ -489,6 +479,19 @@ export default {
       this.ids = selection.map(item => item.id)
       this.single = selection.length!==1
       this.multiple = !selection.length
+    },
+    //公共图片删除
+    handleRemove(file, fileList) {
+      deleteImg(file.id).then(res => {
+        if (res.code == 200) {
+          this.$message({
+            message: "删除成功",
+            type: "success"
+          });
+        } else {
+          this.$message.error("删除失败");
+        }
+      });
     },
     /** 新增按钮操作 */
     handleAdd() {
@@ -596,17 +599,25 @@ export default {
       },
       // 文件上传成功处理
       handleFileSuccess(response, file, fileList) {
-          this.fullscreenLoading = false;
-          this.upload.open = false;
-          this.upload.isUploading = false;
-          this.$refs.upload.clearFiles();
-          this.$alert(response.msg, "导入结果", { dangerouslyUseHTMLString: true });
-          this.getList();
+        this.ifPhotoLimit(fileList.length, " 个文件")
+        this.upload.open = false;
+        this.upload.isUploading = false;
+        console.log(response)
+        this.form.advertisementContent = response.data.url
+        console.log(response.data.url)
+        this.$refs.upload.clearFiles();
       },
+    // 文件限制判断
+    ifPhotoLimit(num, msg) {
+      if (num >= this.maxPhotoNum) {
+        this.$message({
+          message: "最多上传 " + this.maxPhotoNum + msg,
+          type: "warning"
+        });
+      }
+    },
       // 提交上传文件
       submitFileForm() {
-        this.importBtn=true
-        this.fullscreenLoading = true;
         this.$refs.upload.submit();
       }
 
