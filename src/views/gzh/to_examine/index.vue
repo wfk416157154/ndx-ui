6815fb0393778a0779d42767f2b25d16529a6d43
yuanbing216 <>
2022-04-26 17:49:37 +0800

    公众号模块

---
+++ src/views/gzh/to_examine/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        sdfas
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
