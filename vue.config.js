7184b80527fc5e94943f879bbf38a5a57170e74c
YYU <1312646134@qq.com>
2022-06-02 15:06:37 +0800

    target129改成130

--- vue.config.js
+++ vue.config.js
@@ -35,13 +35,14 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://192.168.0.129:9090`,
+        target: `http://192.168.0.130:8080`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
         }
       }
     },
+
     disableHostCheck: true
   },
   configureWebpack: {
